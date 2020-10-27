658c9e936d8b606f70114d2810bfa47e45d6e797
ririyeye <200610237@qq.com>
2020-10-27 10:50:42 +0800

    添加udev&firmware.sh

--- post_patch/post-build.sh
+++ post_patch/post-build.sh
@@ -20,9 +20,17 @@ patch -d ${TARGET_DIR} -p0 < ${POST_DIR}/insmod.patch
 patch -d ${TARGET_DIR} -p0 < ${POST_DIR}/libpath.patch
 patch -d ${TARGET_DIR} -p0 < ${POST_DIR}/nfs.patch
 
-
+#update.sh
 cp ${POST_DIR}/update.sh ${TARGET_DIR}
 chmod a+x ${TARGET_DIR}/update.sh
+
+#udev && firmware.sh
+#udev
+cp ${POST_DIR}/50-udev-default.rules ${TARGET_DIR}/lib/udev/rules.d/50-udev-default.rules
+#firmware.sh
+cp ${POST_DIR}/firmware.sh ${TARGET_DIR}/lib/udev/firmware.sh
+chmod a+x ${TARGET_DIR}/lib/udev/firmware.sh
+
 
 chmod a+x ${TARGET_DIR}/etc/init.d/*
 
